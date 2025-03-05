bf88067cb5753678b220a13587f5f28617b28663
Kanana-Mwenda <Lizamwenda95@gmail.com>
2025-03-05 14:03:21 +0300

    commit message

--- lib/looping.py
+++ lib/looping.py
@@ -12,7 +12,7 @@ def square_integers(int_list):
     return [i ** 2 for i in int_list] 
     
 def fizzbuzz():
-    for i in range(1,10):
+    for i in range(1,101):
         if i%3 == 0 and i%5==0:
             print("FizzBuzz")
         elif i % 3 == 0:
@@ -21,5 +21,3 @@ def fizzbuzz():
             print("Buzz")
         else:
             print(i)
-        
-    pass
